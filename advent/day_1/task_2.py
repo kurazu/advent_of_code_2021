9595aea26b36d73d5ba3e8c8a5eda9ff17b06f8b
Tomasz Maćkowiak <kurazu@kurazu.net>
2021-12-01 21:44:05 +0100

    task 2 implemented using pandas

--- advent/day_1/task_2.py
+++ advent/day_1/task_2.py
@@ -1,7 +1,7 @@
-from collections import deque
-from typing import Deque, Iterator, TextIO
+from typing import TextIO
 
 import click
+import pandas as pd
 
 WINDOW = 3
 
@@ -9,19 +9,11 @@ WINDOW = 3
 @click.command()
 @click.argument("input", type=click.File("r", encoding="utf-8"), required=True)
 def main(input: TextIO) -> None:
-    parsed_numbers: Iterator[int] = iter(map(int, filter(None, input)))
-    previous_observations: Deque[int] = deque(maxlen=WINDOW)
-    for _ in range(WINDOW):
-        previous_observations.append(next(parsed_numbers))
-
-    count = 0
-    previous_sum = sum(previous_observations)
-    for number in parsed_numbers:
-        previous_observations.append(number)
-        current_sum = sum(previous_observations)
-        if current_sum > previous_sum:
-            count += 1
-        previous_sum = current_sum
+    df = pd.read_csv(input, names=["reading"])
+    df["current_sum"] = df.reading.rolling(WINDOW).sum()
+    df["previous_sum"] = df.reading.shift(1).rolling(WINDOW).sum()
+    df.dropna(subset=["previous_sum"], inplace=True)
+    count = (df["current_sum"] > df["previous_sum"]).sum()
     click.echo(f"{count}")
 
 
