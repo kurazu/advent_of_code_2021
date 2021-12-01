2e63a131549bed287778e3e2b288f124e2c9d992
Tomasz Maćkowiak <kurazu@kurazu.net>
2021-12-01 21:28:00 +0100

    second task

--- Makefile
+++ Makefile
@@ -3,3 +3,9 @@ task_1_sample:
 
 task_1_input:
 	poetry run python -m advent.day_1.task_1 data/day_1/task_1/input.txt
+
+task_2_sample:
+	poetry run python -m advent.day_1.task_2 data/day_1/task_1/sample.txt
+
+task_2_input:
+	poetry run python -m advent.day_1.task_2 data/day_1/task_1/input.txt
---
+++ advent/day_1/task_2.py
@@ -0,0 +1,31 @@
+from collections import deque
+from typing import Deque, Iterator, TextIO
+
+import click
+
+WINDOW = 3
+
+
+@click.command()
+@click.argument(
+    "input", type=click.File("r", encoding="utf-8"), required=True, default="-"
+)
+def main(input: TextIO) -> None:
+    parsed_numbers: Iterator[int] = iter(map(int, filter(None, input)))
+    previous_observations: Deque[int] = deque(maxlen=WINDOW)
+    for _ in range(WINDOW):
+        previous_observations.append(next(parsed_numbers))
+
+    count = 0
+    previous_sum = sum(previous_observations)
+    for number in parsed_numbers:
+        previous_observations.append(number)
+        current_sum = sum(previous_observations)
+        if current_sum > previous_sum:
+            count += 1
+        previous_sum = current_sum
+    click.echo(f"{count}")
+
+
+if __name__ == "__main__":
+    main()
