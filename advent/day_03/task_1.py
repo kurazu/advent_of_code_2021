a20366172cdbcc83e2f352954d5863ab589164fc
Tomasz Maćkowiak <kurazu@kurazu.net>
2021-12-03 13:38:25 +0100

    day 3 task 1

--- advent/day_03/task_1.py
+++ advent/day_03/task_1.py
@@ -9,9 +9,19 @@ logger = logging.getLogger(__name__)
 
 
 def main(input: TextIO) -> str:
-    pd.read_csv(input, names="binary")
-    gamma = 0
-    epsilon = 0
+    df = pd.read_csv(input, names=["binary"], dtype={"binary": str})
+    positional_df = df["binary"].apply(
+        lambda binary_number: pd.Series(list(binary_number))
+    )
+
+    most_common_characters = positional_df.apply(
+        lambda column: column.value_counts().sort_values().index[-1], axis="index"
+    )
+    least_common_characters = most_common_characters.map({"0": "1", "1": "0"})
+    epsilon_string = "".join(most_common_characters)
+    epsilon = int(epsilon_string, 2)
+    gamma_string = "".join(least_common_characters)
+    gamma = int(gamma_string, 2)
     logger.info("gamma=%d, epsilon=%d", gamma, epsilon)
     return f"{gamma * epsilon}"
 
