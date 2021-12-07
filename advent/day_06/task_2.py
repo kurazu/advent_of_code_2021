8b8835fb1d813f446f282cc3d8655dcadcac5473
Tomasz Maćkowiak <kurazu@kurazu.net>
2021-12-07 19:02:07 +0100

    day 6 task 2

--- advent/day_06/task_2.py
+++ advent/day_06/task_2.py
@@ -1,4 +1,5 @@
 import logging
+from collections import Counter
 from typing import List, TextIO
 
 import numpy as np
@@ -7,11 +8,21 @@ from ..cli import run_with_file_argument
 
 logger = logging.getLogger(__name__)
 
+DAYS = 256
+MAX_COUNTER = 8
+
 
 def main(input: TextIO) -> str:
-    fish: List[int] = list(map(int, input.readline().strip().split(",")))
-    breakpoint()
-    return f"{0}"
+    fish = np.zeros((MAX_COUNTER + 1,), dtype=int)
+    for status in map(int, input.readline().strip().split(",")):
+        fish[status] += 1
+    for day in range(DAYS):
+        new_fish_count = fish[0]
+        fish[:-1] = fish[1:]
+        fish[-1] = new_fish_count
+        fish[6] += new_fish_count
+    fish_count = np.sum(fish)
+    return f"{fish_count}"
 
 
 if __name__ == "__main__":
