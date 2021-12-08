fa450baf1bca3b9c4fdb87201a211f4014895dc5
Tomasz Maćkowiak <kurazu@kurazu.net>
2021-12-08 16:22:22 +0100

    day 8 task 2

--- advent/day_08/task_2.py
+++ advent/day_08/task_2.py
@@ -35,12 +35,13 @@ def resolve(
 
     # From 6-segment digits (0, 9 and 6) only 6 will not have 2 common parts with 1.
     # Thus we can figure out which signal is 6.
-    for garbled in garbled_by_length[SegmentCount(6)]:
+    search_space = garbled_by_length[SegmentCount(6)]
+    for garbled in search_space:
         common_part = resolved_digits[Digit(1)] & garbled
         if len(common_part) == 1:
             # we found 6
             resolved_digits[Digit(6)] = garbled
-            garbled_by_length[SegmentCount(6)].remove(garbled)
+            search_space.remove(garbled)
             # We can also resolve segments C and F
             (f_segment,) = common_part
             (c_segment,) = resolved_digits[Digit(1)] - common_part
@@ -50,17 +51,53 @@ def resolve(
 
     # From 5-segment digits (2, 3, 5) only 3 will have 2 common parts with 1.
     # Thus we can figure out which signal is 3.
-    for garbled in garbled_by_length[SegmentCount(5)]:
+    search_space = garbled_by_length[SegmentCount(5)]
+    for garbled in search_space:
         common_part = resolved_digits[Digit(1)] & garbled
         if len(common_part) == 2:
             # we cound 3
             resolved_digits[Digit(3)] = garbled
-            garbled_by_length[SegmentCount(5)].remove(garbled)
+            search_space.remove(garbled)
             break
     else:
         raise AssertionError("3 not found")
-    breakpoint()
-    return [Digit(0), Digit(0), Digit(0), Digit(0)]
+
+    # From remaining 5-segment digits (2, 5) only 2 will have C-segment lit up.
+    # We can use that to figure out 2 and 5.
+    search_space = garbled_by_length[SegmentCount(5)]
+    for garbled in search_space:
+        if c_segment in garbled:
+            # We found 2
+            resolved_digits[Digit(2)] = garbled
+            search_space.remove(garbled)
+            # That means that the ramaining one will be 5
+            (resolved_digits[Digit(5)],) = search_space
+            break
+    else:
+        raise AssertionError("2 not found")
+
+    # We can also now figure out the E segment
+    (e_segment,) = resolved_digits[Digit(2)] - resolved_digits[Digit(3)]
+
+    # The last assignment is to distinguish between remaining 6-segment digits (0 and 9)
+    # From those only 0 will have segment E lit up.
+    search_space = garbled_by_length[SegmentCount(6)]
+    for garbled in search_space:
+        if e_segment in garbled:
+            # we have found 0
+            resolved_digits[Digit(0)] = garbled
+            search_space.remove(garbled)
+            # the remaining one will be 9 then
+            (resolved_digits[Digit(9)],) = search_space
+            break
+    else:
+        raise AssertionError("0 not found")
+
+    # invert the mapping so that we can get digit from garbled signal
+    resolved_garbled = {garbled: digit for digit, garbled in resolved_digits.items()}
+
+    # We have resolved all the digits, so we can use them to decipher the outputs
+    return [resolved_garbled[garbled] for garbled in outputs]
 
 
 def main(input: TextIO) -> str:
