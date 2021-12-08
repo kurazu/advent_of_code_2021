674f3aa620145f90b531193e4952acb079184759
Tomasz Maćkowiak <kurazu@kurazu.net>
2021-12-08 11:53:13 +0100

    day 8 task 1 WIP

--- advent/day_08/task_1.py
+++ advent/day_08/task_1.py
@@ -1,5 +1,7 @@
+import enum
 import logging
-from typing import List, TextIO
+from collections import defaultdict
+from typing import Dict, List, NewType, Set, TextIO
 
 import numpy as np
 
@@ -7,20 +9,60 @@ from ..cli import run_with_file_argument
 
 logger = logging.getLogger(__name__)
 
+Digit = NewType("Digit", int)
+SegmentCount = NewType("SegmentCount", int)
+
+
+class Segment(str, enum.Enum):
+    A = "a"
+    B = "b"
+    C = "c"
+    D = "d"
+    E = "e"
+    F = "f"
+    G = "g"
+
+
+# How many segments are needed to display a given digit
+SEGMENTS_BY_DIGIT: Dict[Digit, SegmentCount] = {
+    Digit(0): SegmentCount(6),
+    Digit(1): SegmentCount(2),
+    Digit(2): SegmentCount(5),
+    Digit(3): SegmentCount(5),
+    Digit(4): SegmentCount(4),
+    Digit(5): SegmentCount(5),
+    Digit(6): SegmentCount(6),
+    Digit(7): SegmentCount(3),
+    Digit(8): SegmentCount(7),
+    Digit(9): SegmentCount(6),
+}
+
+
+DIGIT_BY_SEGMENTS: Dict[SegmentCount, Set[Digit]] = defaultdict(set)
+for digit, count in SEGMENTS_BY_DIGIT.items():
+    DIGIT_BY_SEGMENTS[count].add(digit)
+
+EASY_DIGITS = {
+    count: next(iter(digits))
+    for count, digits in DIGIT_BY_SEGMENTS.items()
+    if len(digits) == 1
+}
+
 
 def main(input: TextIO) -> str:
+    logging.info("Easy digits: %s", EASY_DIGITS)
     stripped_lines = (line.strip() for line in input)
     non_empty_stripped_lines = filter(None, stripped_lines)
-    all_signals: List[List[str]] = []
-    all_outputs: List[List[str]] = []
+    all_signals: List[List[Set[Segment]]] = []
+    all_outputs: List[List[Set[Segment]]] = []
     for line in non_empty_stripped_lines:
         _signals, _outputs = line.split(" | ")
         signals = _signals.split(" ")
         outputs = _outputs.split(" ")
         assert len(signals) == 10
         assert len(outputs) == 4
-        all_signals.append(signals)
-        all_outputs.append(outputs)
+        all_signals.append([{Segment(s) for s in signal} for signal in signals])
+        all_outputs.append([{Segment(o) for o in output} for output in outputs])
     breakpoint()
     occurrences = 0
     return f"{occurrences}"
