c6b23f117fd43b49156477fc8bf26a911c491262
Tomasz Maćkowiak <kurazu@kurazu.net>
2021-12-11 19:22:06 +0100

    day 11 task 1 WIP

--- advent/day_11/task_1.py
+++ advent/day_11/task_1.py
@@ -16,27 +16,31 @@ def read_input(input: TextIO) -> npt.NDArray[int]:
 
 STEPS = 3
 PADDING = 1
+MAX_ENERGY = 9
 
 
 def main(input: TextIO) -> str:
     octopusses = read_input(input)
     padded_octopusses = np.pad(octopusses, [(PADDING, PADDING), (PADDING, PADDING)])
     octopusses = padded_octopusses[PADDING:-PADDING, PADDING:-PADDING]
+
+    logger.info("Before any steps:\n%s", octopusses)
+
     flashes = 0
     for step in range(STEPS):
         # increase power level of all octopusses by 1
         octopusses += 1
 
         # a mask of octopusses that did not yet flash this step
-        not_flashed = np.ones_like(octopusses)
+        can_still_flash = np.ones_like(octopusses)
 
         while True:
             # find the octopusses that need to flash
-            flashing: npt.NDArray[bool] = octopusses >= 9
+            flashing: npt.NDArray[bool] = octopusses > MAX_ENERGY
 
             # the ones that we will flash are the ones that have the power and have
             # not yet flashed this step
-            flash: npt.NDArray[bool] = flashing & not_flashed
+            flash: npt.NDArray[bool] = flashing & can_still_flash
 
             indices = np.argwhere(flash)
             if not len(indices):
@@ -46,13 +50,15 @@ def main(input: TextIO) -> str:
                 padded_octopusses[
                     y - 1 + PADDING : y + 1 + PADDING, x - 1 + PADDING : x + 1 + PADDING
                 ] += 1
+                # Mark the ones that flashed and remove them from the mask
+                can_still_flash[y, x] = False
+                # Count number of flashes
                 flashes += 1
 
-            # Mark the ones that flashed and remove them from the mask
-            not_flashed[flash] = True
+        # Zero any octopusses that flashed
+        octopusses[octopusses > MAX_ENERGY] = 0
 
         logger.info("Step %d:\n%s", step + 1, octopusses)
-    flashes = 0
     return f"{flashes}"
 
 
