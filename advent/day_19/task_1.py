e678515d72847a01683dc6a657468d5d12946cdc
Tomasz Maćkowiak <kurazu@kurazu.net>
2021-12-19 13:30:43 +0100

    day 19 task 1 determine which scanners overlap with each other

--- advent/day_19/task_1.py
+++ advent/day_19/task_1.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
 import logging
+import math
 import re
 from collections import Counter
 from itertools import combinations, starmap
-from typing import Iterable, List, TextIO, Tuple
+from typing import Dict, Iterable, List, TextIO, Tuple
 
 import numpy as np
 import numpy.typing as npt
@@ -35,9 +36,20 @@ def distance(a: npt.NDArray[int], b: npt.NDArray[int]) -> float:
     return dist
 
 
+def get_edges(beacons: npt.NDArray[int]) -> Dict[float, Tuple[int, int]]:
+    enumerated_beacons = enumerate(beacons)
+    result: Dict[float, Tuple[int, int]] = {}
+    for (a_idx, a_beacon), (b_idx, b_beacon) in combinations(enumerated_beacons, 2):
+        dist = distance(a_beacon, b_beacon)
+        assert dist not in result
+        result[dist] = a_idx, b_idx
+    return result
+
+
 def main(input: TextIO) -> str:
     scanners = list(read_beacons(input))
 
+    has_repeating_distances = False
     for i, beacons in enumerate(scanners):
         distances = starmap(distance, combinations(beacons, 2))
         counter = Counter(distances)
@@ -46,7 +58,23 @@ def main(input: TextIO) -> str:
         )
         for count in repeating_distances:
             logger.info("Scanner %d repeated distance %d", i, count)
+            has_repeating_distances = True
         logger.info("Scanner %d done", i)
+    assert not has_repeating_distances
+
+    # if 12 nodes need to overlap, then COMB(12,2) edges must math (size of clique edges)
+    min_edges = math.comb(12, 2)
+    for (a_idx, a), (b_idx, b) in combinations(enumerate(scanners), 2):
+        first_edges = get_edges(a)
+        second_edges = get_edges(b)
+        common_edges = set(first_edges) & set(second_edges)
+        if len(common_edges) >= min_edges:
+            logger.info(
+                "Scanner %d and %d have %d common edges",
+                a_idx,
+                b_idx,
+                len(common_edges),
+            )
 
     number_of_beacons = 0
     return f"{number_of_beacons}"
