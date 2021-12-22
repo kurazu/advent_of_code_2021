84f026d198ec26d33fc59eae2c94c145dc0311ed
Tomasz Maćkowiak <kurazu@kurazu.net>
2021-12-22 14:11:42 +0100

    day 20 task 1

--- advent/day_20/task_1.py
+++ advent/day_20/task_1.py
@@ -36,8 +36,40 @@ def format_image(image: npt.NDArray[int]) -> str:
     )
 
 
-def enhance_image(image: npt.NDArray[int], algorithm: npt.NDArray[int]) -> npt.NDArray:
-    return image
+POSITION_DECODER = 2 ** (np.arange(9)[::-1])
+
+
+def enhance_pixel(square: npt.NDArray[int], algorithm: npt.NDArray[int]) -> int:
+    assert square.shape == (3, 3)
+    index = np.sum(np.ravel(square) * POSITION_DECODER)
+    pixel_value: int = algorithm[index]
+    return pixel_value
+
+
+def enhance_image(
+    image: npt.NDArray[int], algorithm: npt.NDArray[int], fill_value: int
+) -> npt.NDArray[int]:
+    height, width = image.shape
+    padding = 1
+    # We pad the image so that we can take points also around the edge pixels
+    extruded_image = np.pad(
+        image,
+        [(padding * 2, padding * 2), (padding * 2, padding * 2)],
+        mode="constant",
+        constant_values=fill_value,
+    )
+
+    target_image_height = height + 2 * padding
+    target_image_width = width + 2 * padding
+    target_image = np.zeros((target_image_height, target_image_width), dtype=int)
+
+    for y in range(target_image_height):
+        for x in range(target_image_width):
+            square = extruded_image[
+                y + padding - 1 : y + padding + 2, x + padding - 1 : x + padding + 2
+            ]
+            target_image[y, x] = enhance_pixel(square, algorithm)
+    return target_image
 
 
 def main(input: TextIO) -> str:
@@ -46,14 +78,13 @@ def main(input: TextIO) -> str:
     image = read_image(input)
     logger.info("Input image:\n%s", format_image(image))
 
-    image = enhance_image(image, algorithm)
+    image = enhance_image(image, algorithm, 0)
     logger.info("Enhanced image:\n%s", format_image(image))
 
-    image = enhance_image(image, algorithm)
+    image = enhance_image(image, algorithm, 1)
     logger.info("Twice enhanced image:\n%s", format_image(image))
 
-    breakpoint()
-    pixels_lit = 0
+    pixels_lit = np.sum(image)
     return f"{pixels_lit}"
 
 
