0314291bebe039e695ca666f9c0ce7f3aa80d40f
Tomasz Maćkowiak <kurazu@kurazu.net>
2022-01-02 22:28:25 +0100

    day 23 task 1 WIP

--- advent/day_23/task_1.py
+++ advent/day_23/task_1.py
@@ -1,16 +1,13 @@
 from __future__ import annotations
 
 import logging
+import operator
 import re
 from dataclasses import dataclass
 from enum import Enum
-from typing import Iterable, Optional, TextIO
-
-import numpy as np
-import numpy.typing as npt
+from typing import Iterable, List, NamedTuple, Optional, Set, TextIO, Tuple
 
 from ..cli import run_with_file_argument
-from ..io_utils import get_lines
 
 logger = logging.getLogger(__name__)
 
@@ -59,15 +56,39 @@ class Board:
     DH: Optional[Amphipod] = None
     DL: Optional[Amphipod] = None
 
+    @staticmethod
+    def format_amphipod(amphipod: Optional[Amphipod]) -> str:
+        return amphipod.value if amphipod is not None else " "
+
     def __repr__(self) -> str:
-        def f(amphipod: Optional[Amphipod]) -> str:
-            return amphipod.value if amphipod is not None else " "
+        f = self.format_amphipod
 
         return f"""#############
 #{f(self.LF)}{f(self.LN)} {f(self.AB)} {f(self.BC)} {f(self.CD)} {f(self.RN)}{f(self.RF)}#
 ###{f(self.AH)}#{f(self.BH)}#{f(self.CH)}#{f(self.DH)}###
   #{f(self.AL)}#{f(self.BL)}#{f(self.CL)}#{f(self.DL)}#
   #########"""
+
+    def id(self) -> str:
+        f = self.format_amphipod
+
+        return (
+            f"{f(self.LF)}{f(self.LN)}{f(self.AB)}{f(self.BC)}{f(self.CD)}{f(self.RN)}{f(self.RF)}"
+            f"{f(self.AH)}{f(self.BH)}{f(self.CH)}{f(self.DH)}"
+            f"{f(self.AL)}{f(self.BL)}{f(self.CL)}{f(self.DL)}"
+        )
+
+
+TARGET_BOARD = Board(
+    AH=Amphipod.AMBER,
+    AL=Amphipod.AMBER,
+    BH=Amphipod.BRONZE,
+    BL=Amphipod.BRONZE,
+    CH=Amphipod.COPPER,
+    CL=Amphipod.COPPER,
+    DH=Amphipod.DESERT,
+    DL=Amphipod.DESERT,
+)
 
 
 def read_board(input: TextIO) -> Board:
@@ -79,10 +100,66 @@ def read_board(input: TextIO) -> Board:
     return Board(**parsed_groups)
 
 
+class DFSResult(NamedTuple):
+    steps: List[Board]
+    total_energy: int
+
+
+def get_possible_moves(board: Board) -> Iterable[Tuple[Board, int]]:
+    return []
+
+
+Stack = List[Board]
+
+
+def dfs(starting_board: Board) -> Tuple[Stack, int]:
+    best_energy = float("inf")
+    boards_seen: Set[str] = set()
+
+    def recursive_search(
+        stack: Stack, stack_energy: int
+    ) -> Optional[Tuple[Stack, int]]:
+        nonlocal best_energy
+
+        *_, current_board = stack
+        boards_seen.add(current_board.id())
+        if current_board == TARGET_BOARD:
+            # Terminal state
+            best_energy = min(best_energy, stack_energy)
+            return stack, stack_energy
+        else:
+            possibilities: List[Tuple[Stack, int]] = []
+            for possible_board, move_energy in get_possible_moves(current_board):
+                possible_board_id = possible_board.id()
+                possible_energy = stack_energy + move_energy
+                if possible_board_id in boards_seen:
+                    continue  # We've seen this state
+                elif possible_energy >= best_energy:
+                    continue  # This state is already worse than the current best
+                else:  # State to explore
+                    possible_result = recursive_search(
+                        stack + [possible_board], possible_energy
+                    )
+                    if possible_result is None:
+                        continue
+                    else:
+                        possibilities.append(possible_result)
+            if not possibilities:
+                return None
+            else:
+                possibilities.sort(key=operator.itemgetter(1))
+                return possibilities[0]
+
+    best_result = recursive_search([starting_board], 0)
+    assert best_result is not None
+    return best_result
+
+
 def main(input: TextIO) -> str:
     starting_board = read_board(input)
-    breakpoint()
-    energy = 0
+    stack, energy = dfs(starting_board)
+    for board in stack:
+        logger.info("Step\n%r\n", board)
     return f"{energy}"
 
 
